a14db5fe2aa307f19e1853c9f3da1c4419dbe0bd
Scotty-Did-Does <scott.neely.design@gmail.com>
2022-11-17 10:29:32 +1100

    Added more turrets. Created a turret script that allows for 360 rotation. Prefabs for turrets and ui created.

--- ProjectSkyJavlin/Assets/Scripts/CameraController.cs
+++ ProjectSkyJavlin/Assets/Scripts/CameraController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
@@ -9,16 +9,25 @@ public class CameraController : MonoBehaviour
     [SerializeField] private KeyCode turret0 = KeyCode.Alpha1;
     [SerializeField] private KeyCode turret1 = KeyCode.Alpha2;
     [SerializeField] private KeyCode turret2 = KeyCode.Alpha3;
+    [SerializeField] private KeyCode turret3 = KeyCode.Alpha4;
+    [SerializeField] private KeyCode turret4 = KeyCode.Alpha5;
 
     //[SerializeField] private KeyCode cycleNext = KeyCode.E;
     //[SerializeField] private KeyCode cycleLast = KeyCode.Q;
 
-
     public List<GameObject> cameraList;
+
+    public List<GameObject> fakeTurrets;
 
    public void Start()
     {
         cameraList[0].SetActive(true);
+        
+        fakeTurrets[0].SetActive(false);
+        fakeTurrets[1].SetActive(true);
+        fakeTurrets[2].SetActive(true);
+        fakeTurrets[3].SetActive(true);
+        fakeTurrets[4].SetActive(true);
     }
 
     public void Update()
@@ -34,22 +43,76 @@ public class CameraController : MonoBehaviour
             cameraList[0].SetActive(true);
             cameraList[1].SetActive(false);
             cameraList[2].SetActive(false);
-            
+            cameraList[3].SetActive(false);
+            cameraList[4].SetActive(false);
+
+            fakeTurrets[0].SetActive(false);
+            fakeTurrets[1].SetActive(true);
+            fakeTurrets[2].SetActive(true);
+            fakeTurrets[3].SetActive(true);
+            fakeTurrets[4].SetActive(true);
         }
 
         if (Input.GetKey(turret1))
         {
             Debug.Log("Turret Left");
-            cameraList[1].SetActive(true);
             cameraList[0].SetActive(false);
+            cameraList[1].SetActive(true);
             cameraList[2].SetActive(false);
+            cameraList[3].SetActive(false);
+            cameraList[4].SetActive(false);
+
+            fakeTurrets[0].SetActive(true);
+            fakeTurrets[1].SetActive(false);
+            fakeTurrets[2].SetActive(true);
+            fakeTurrets[3].SetActive(true);
+            fakeTurrets[4].SetActive(true);
         }
 
         if (Input.GetKey(turret2))
         {
-            cameraList[2].SetActive(true);
             cameraList[0].SetActive(false);
             cameraList[1].SetActive(false);
+            cameraList[2].SetActive(true);
+            cameraList[3].SetActive(false);
+            cameraList[4].SetActive(false);
+
+            fakeTurrets[0].SetActive(true);
+            fakeTurrets[1].SetActive(true);
+            fakeTurrets[2].SetActive(false);
+            fakeTurrets[3].SetActive(true);
+            fakeTurrets[4].SetActive(true);
+        }
+
+
+        if (Input.GetKey(turret3))
+        {
+            cameraList[0].SetActive(false);
+            cameraList[1].SetActive(false);
+            cameraList[2].SetActive(false);
+            cameraList[3].SetActive(true);
+            cameraList[4].SetActive(false);
+
+            fakeTurrets[0].SetActive(true);
+            fakeTurrets[1].SetActive(true);
+            fakeTurrets[2].SetActive(true);
+            fakeTurrets[3].SetActive(false);
+            fakeTurrets[4].SetActive(true);
+        }
+
+        if (Input.GetKey(turret4))
+        {
+            cameraList[0].SetActive(false);
+            cameraList[1].SetActive(false);
+            cameraList[2].SetActive(false);
+            cameraList[3].SetActive(false);
+            cameraList[4].SetActive(true);
+
+            fakeTurrets[0].SetActive(true);
+            fakeTurrets[1].SetActive(true);
+            fakeTurrets[2].SetActive(true);
+            fakeTurrets[3].SetActive(true);
+            fakeTurrets[4].SetActive(false);
         }
     }
 }
---
+++ ProjectSkyJavlin/Assets/Scripts/HemiTurretControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HemiTurretControl : MonoBehaviour
+{
+    [Header("CameraPerameters")]
+    [SerializeField, Range(1, 100)] public float lookSpeedX = 2.0f;
+    [SerializeField, Range(1, 100)] public float lookSpeedY = 2.0f;
+   // [SerializeField] public float aimLimitPosY;
+    [SerializeField] public float aimLimitPosX;
+    //[SerializeField] public float aimLimitNegY;
+    [SerializeField] public float aimLimitNegX;
+
+    public Camera camera;
+
+    private float rotationX = 0;
+    private float rotationY = 0;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMouseLook();
+    }
+
+    void HandleMouseLook()
+    {
+        rotationX -= Input.GetAxis("Mouse Y") * lookSpeedY;
+        rotationX = Mathf.Clamp(rotationX, aimLimitPosX, aimLimitNegX);
+        rotationY += Input.GetAxis("Mouse X") * lookSpeedX;
+        //rotationY = Mathf.Clamp(rotationY, aimLimitPosY, aimLimitNegY);
+        camera.transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0);
+    }
+}
